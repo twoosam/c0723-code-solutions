20b265efeab648dca29b68ac3bfe228eac609727
Tim Woo-Sam <twoosam10@gmail.com>
2023-07-12 05:05:20 +0000

    Completed javascript-functions-practice exercise.

---
+++ javascript-functions-practice/get-description-of-person.js
@@ -0,0 +1,12 @@
+/* exported getDescriptionOfPerson */
+
+function getDescriptionOfPerson(person) {
+  return (
+    person.name +
+    ' is a ' +
+    person.occupation +
+    ' from ' +
+    person.birthPlace +
+    '.'
+  );
+}
