29215266c8fc2030c3c51c8b7545f5a68a734175
Tim Woo-Sam <twoosam10@gmail.com>
2023-07-14 16:30:24 +0000

    Completed string-manipulation-1 exercise.

---
+++ string-manipulation-1/src/is-vowel.js
@@ -0,0 +1,20 @@
+/* exported isVowel */
+
+function isVowel(char) {
+  if (
+    char === 'a' ||
+    char === 'e' ||
+    char === 'i' ||
+    char === 'o' ||
+    char === 'u' ||
+    char === 'A' ||
+    char === 'E' ||
+    char === 'I' ||
+    char === 'O' ||
+    char === 'U'
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
