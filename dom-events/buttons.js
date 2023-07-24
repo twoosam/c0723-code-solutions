48a9febc47561277dba4e8ce62a51d8f07938045
Tim Woo-Sam <twoosam10@gmail.com>
2023-07-24 22:04:25 +0000

    Added $ before variable names.

--- dom-events/buttons.js
+++ dom-events/buttons.js
@@ -4,8 +4,8 @@ function handleClick(event) {
   console.log('event.target:', event.target);
 }
 
-const buttonClick = document.querySelector('.click-button');
-buttonClick.addEventListener('click', handleClick);
+const $buttonClick = document.querySelector('.click-button');
+$buttonClick.addEventListener('click', handleClick);
 
 function handleMouseover(event) {
   console.log('button hovered');
@@ -13,8 +13,8 @@ function handleMouseover(event) {
   console.log('event.target:', event.target);
 }
 
-const buttonHover = document.querySelector('.hover-button');
-buttonHover.addEventListener('mouseover', handleMouseover);
+const $buttonHover = document.querySelector('.hover-button');
+$buttonHover.addEventListener('mouseover', handleMouseover);
 
 function handleDoubleClick(event) {
   console.log('button double-clicked');
@@ -22,5 +22,5 @@ function handleDoubleClick(event) {
   console.log('event.target:', event.target);
 }
 
-const buttonDoubleClick = document.querySelector('.double-click-button');
-buttonDoubleClick.addEventListener('dblclick', handleDoubleClick);
+const $buttonDoubleClick = document.querySelector('.double-click-button');
+$buttonDoubleClick.addEventListener('dblclick', handleDoubleClick);
