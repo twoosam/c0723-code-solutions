5da1d311bce5ea49ae3d64ed60d6af7f4cc29b7d
Tim Woo-Sam <twoosam10@gmail.com>
2023-08-25 04:43:38 +0000

    Saved changes to react-events.

---
+++ react-events/my-app/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import CustomButton from './CustomButton';
+
+function handleCustomClick(text) {
+  alert(text);
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <CustomButton onCustomClick={handleCustomClick} text="I" color="red" />
+      <CustomButton
+        onCustomClick={handleCustomClick}
+        text="know"
+        color="green"
+      />
+      <CustomButton
+        onCustomClick={handleCustomClick}
+        text="React!"
+        color="blue"
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ react-events/my-app/src/CustomButton.jsx
@@ -0,0 +1,7 @@
+export default function CustomButton({ text, color, onCustomClick }) {
+  return (
+    <button onClick={onCustomClick(text)} style={{ backgroundColor: color }}>
+      {text}
+    </button>
+  );
+}
