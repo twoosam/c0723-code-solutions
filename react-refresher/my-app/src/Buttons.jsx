bfa5585161546b3b8967292ecb68c27eafe2d3f6
Tim Woo-Sam <twoosam10@gmail.com>
2023-08-29 03:52:11 +0000

    Completed react-refresher exercise.

--- react-refresher/my-app/src/Buttons.jsx
+++ react-refresher/my-app/src/Buttons.jsx
@@ -2,5 +2,11 @@ import { useState } from 'react';
 
 export default function Buttons() {
   const [count, setCount] = useState(0);
-  return <button onClick={setCount(count - 1)}>Down</button>;
+  return (
+    <div>
+      <button onClick={() => setCount(count - 1)}>Down</button>
+      {count}
+      <button onClick={() => setCount(count + 1)}>Up</button>
+    </div>
+  );
 }
