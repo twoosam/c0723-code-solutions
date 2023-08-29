00319f5d2029142f46f1b9cd8a2dbcaed84940ce
Tim Woo-Sam <twoosam10@gmail.com>
2023-08-29 22:26:30 +0000

    Completed react-rendering-lists exercise.

---
+++ react-rendering-lists/my-app/src/PokemonList.jsx
@@ -0,0 +1,6 @@
+export default function PokemonList({ pokedex }) {
+  const listItems = pokedex.map((pokemon) => (
+    <li key={pokemon.number}>{pokemon.name}</li>
+  ));
+  return <ul>{listItems}</ul>;
+}
