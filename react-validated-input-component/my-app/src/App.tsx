6f114cc1ea61209eea16de502fe89b8b976212df
Tim Woo-Sam <twoosam10@gmail.com>
2023-08-30 22:18:48 +0000

    Replaced uncessary props with local variables.

--- react-validated-input-component/my-app/src/App.tsx
+++ react-validated-input-component/my-app/src/App.tsx
@@ -1,11 +1,8 @@
 import ValidatedInput from './ValidatedInput';
-import { FcCancel } from 'react-icons/fc';
 import './App.css';
 
 function App() {
-  return (
-    <ValidatedInput underText="A password is required." symbol={<FcCancel />} />
-  );
+  return <ValidatedInput />;
 }
 
 export default App;
--- react-validated-input-component/my-app/src/ValidatedInput.jsx
+++ react-validated-input-component/my-app/src/ValidatedInput.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react';
 import { FcCheckmark } from 'react-icons/fc';
 import { FcCancel } from 'react-icons/fc';
 
-export default function ValidatedInput({ underText, symbol }) {
+export default function ValidatedInput() {
   const [password, setPassword] = useState('');
-  if (password.length > 0 && password.length < 8) {
+  let underText = null;
+  let symbol = null;
+  if (password.length === 0) {
+    underText = 'A password is required.';
+    symbol = <FcCancel />;
+  } else if (password.length < 8) {
     underText = 'Your password is too short.';
     symbol = <FcCancel />;
   } else if (password.length >= 8) {
