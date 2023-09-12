36e81caa37d9ac0251eb7160fcb856d5b11170e5
Tim Woo-Sam <twoosam10@gmail.com>
2023-09-12 15:23:25 +0000

    Saved changes to express-post-json.

---
+++ express-post-json/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92,
+  },
+};
+
+const app = express();
+
+// let nextId = 1;
+
+app.use(express.json());
+
+app.get('/api/grades', (req, res) => {
+  console.log(req.method);
+  const array = [];
+  for (const key in grades) {
+    array.push(grades[key]);
+  }
+  res.json(array);
+});
+
+// app.post('/api/grades', (req, res) => {});
+
+app.listen(8080, () => {
+  console.log('Express server listening on port 8080');
+});
