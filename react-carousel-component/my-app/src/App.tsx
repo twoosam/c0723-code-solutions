9d69a26b2e8372bc339baa16282092522a3cba97
Tim Woo-Sam <twoosam10@gmail.com>
2023-09-14 23:04:51 +0000

    Completed react-carousel-component exercise.

---
+++ react-carousel-component/my-app/src/App.tsx
@@ -0,0 +1,9 @@
+import Carousel from './Carousel';
+import './App.css';
+
+const images = ['001', '004', '007', '025', '039'];
+
+function App() {
+  return <Carousel images={images} />;
+}
+export default App;
