1151c0cb12b77fa6046a3b41b5ae4acd9c3f2a4b
Tim Woo-Sam <twoosam10@gmail.com>
2023-09-22 03:03:29 +0000

    Completed api-authorization exercise.

---
+++ api-authorization/typed/authorization-middleware.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars -- Remove me */
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { ClientError } from './client-error.js';
+
+export function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  /* your code here */
+}
+
+/*
+ * Get the 'Authorization' header from the request.
+ * Parse the token from the header; e.g., auth.split('Bearer ')[1]
+ * If no header or no token is provided,
+ *   throw a 401 error with the message 'authentication required'
+ * Use jwt.verify() to verify the authenticity of the token and extract its payload.
+ * Assign the extracted payload to the user property of the req object.
+ * Call next() (with no arguments) to let Express know to advance to its next route or middleware.
+ *
+ * References:
+ * https://expressjs.com/en/4x/api.html#req.get
+ * https://nodejs.org/api/http.html#http_message_headers
+ * https://github.com/auth0/node-jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
+ */
